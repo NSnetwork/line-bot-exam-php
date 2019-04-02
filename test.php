a64c5377c3812fb3731256eb91c78088f0e09a2f
Manasthep <46101491+Manasthep@users.noreply.github.com>
2019-04-02 13:44:54 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello LINE BOT";
+echo "<br>";
+echo "Hello LINE BOT2";
+echo "<br>";
+echo "Hello LINE BOT3";
+echo "<br>";
+echo "Hello LINE BOT4";
+echo "<br>";
+echo "Hello LINE BOT5";
+?>
